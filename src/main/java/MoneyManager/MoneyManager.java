af2eea2c801aa706227dd4b0fafc13f27a55ca17
XYF <xieyifeng@aliyun.com>
2024-05-13 15:55:42 +0800

    feat:我吃柠檬

--- src/main/java/MoneyManager/MoneyManager.java
+++ src/main/java/MoneyManager/MoneyManager.java
@@ -50,8 +50,8 @@ class LoginFrame extends JFrame implements ActionListener{
     }
     public void actionPerformed(ActionEvent e) {
         if(b_cancel==e.getSource()){
-            this.dispose();
             //添加退出代码
+            this.dispose();
         }else if(b_ok==e.getSource()){
             //添加代码，验证身份成功后显示主界面
             String name = t_user.getText().trim();
